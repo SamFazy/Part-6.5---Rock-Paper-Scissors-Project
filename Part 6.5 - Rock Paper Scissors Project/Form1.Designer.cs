9d9ca85800469784131989b9cfab088d55b98150
Sam F <samfaz10@hotmail.com>
2021-10-07 12:53:52 -0400

    Add project files.

---
+++ Part 6.5 - Rock Paper Scissors Project/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Part_6._5___Rock_Paper_Scissors_Project
+{
+    partial class frmRockPaperScissors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRockPaperScissors));
+            this.picRock = new System.Windows.Forms.PictureBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.picPaper = new System.Windows.Forms.PictureBox();
+            this.picScissors = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPc = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picRock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picScissors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picRock
+            // 
+            this.picRock.BackColor = System.Drawing.Color.Transparent;
+            this.picRock.Image = ((System.Drawing.Image)(resources.GetObject("picRock.Image")));
+            this.picRock.Location = new System.Drawing.Point(119, 108);
+            this.picRock.Name = "picRock";
+            this.picRock.Size = new System.Drawing.Size(150, 150);
+            this.picRock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picRock.TabIndex = 0;
+            this.picRock.TabStop = false;
+            this.picRock.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.BackColor = System.Drawing.Color.Transparent;
+            this.lblInstructions.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.ForeColor = System.Drawing.Color.White;
+            this.lblInstructions.Location = new System.Drawing.Point(237, 19);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(347, 31);
+            this.lblInstructions.TabIndex = 3;
+            this.lblInstructions.Text = "Choose Rock, Paper, or Scissors";
+            // 
+            // picPaper
+            // 
+            this.picPaper.Image = ((System.Drawing.Image)(resources.GetObject("picPaper.Image")));
+            this.picPaper.Location = new System.Drawing.Point(334, 108);
+            this.picPaper.Name = "picPaper";
+            this.picPaper.Size = new System.Drawing.Size(150, 150);
+            this.picPaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPaper.TabIndex = 4;
+            this.picPaper.TabStop = false;
+            // 
+            // picScissors
+            // 
+            this.picScissors.Image = ((System.Drawing.Image)(resources.GetObject("picScissors.Image")));
+            this.picScissors.Location = new System.Drawing.Point(556, 108);
+            this.picScissors.Name = "picScissors";
+            this.picScissors.Size = new System.Drawing.Size(150, 150);
+            this.picScissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picScissors.TabIndex = 5;
+            this.picScissors.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(119, 325);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(334, 325);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(556, 325);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer1.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.ForeColor = System.Drawing.Color.Lime;
+            this.lblPlayer1.Location = new System.Drawing.Point(357, 74);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(101, 31);
+            this.lblPlayer1.TabIndex = 9;
+            this.lblPlayer1.Text = "Player 1";
+            // 
+            // lblPc
+            // 
+            this.lblPc.AutoSize = true;
+            this.lblPc.BackColor = System.Drawing.Color.Transparent;
+            this.lblPc.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPc.ForeColor = System.Drawing.Color.Yellow;
+            this.lblPc.Location = new System.Drawing.Point(385, 291);
+            this.lblPc.Name = "lblPc";
+            this.lblPc.Size = new System.Drawing.Size(42, 31);
+            this.lblPc.TabIndex = 10;
+            this.lblPc.Text = "PC";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.Color.Transparent;
+            this.lblResult.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.White;
+            this.lblResult.Location = new System.Drawing.Point(12, 19);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(86, 31);
+            this.lblResult.TabIndex = 11;
+            this.lblResult.Text = "Result:";
+            // 
+            // frmRockPaperScissors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(832, 561);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblPc);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.picScissors);
+            this.Controls.Add(this.picPaper);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.picRock);
+            this.Name = "frmRockPaperScissors";
+            this.Text = "Rock Paper Scissors";
+            ((System.ComponentModel.ISupportInitialize)(this.picRock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picScissors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picRock;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.PictureBox picPaper;
+        private System.Windows.Forms.PictureBox picScissors;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPc;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Part 6.5 - Rock Paper Scissors Project/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_6._5___Rock_Paper_Scissors_Project
+{
+    public partial class frmRockPaperScissors : Form
+    {
+        public frmRockPaperScissors()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (picPaper.Click == true)
+            {
+
+            }
+        }
+    }
+}
