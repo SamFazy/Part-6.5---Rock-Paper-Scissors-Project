c123e3e11bcafbf5f90623df2a05f547f147ede6
Sam F <samfaz10@hotmail.com>
2021-10-07 14:25:43 -0400

    Done

--- Part 6.5 - Rock Paper Scissors Project/Form1.Designer.cs
+++ Part 6.5 - Rock Paper Scissors Project/Form1.Designer.cs
@@ -36,9 +36,17 @@
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.lblPlayer1 = new System.Windows.Forms.Label();
-            this.lblPc = new System.Windows.Forms.Label();
+            this.lblPlayer1Middle = new System.Windows.Forms.Label();
+            this.lblPcMiddle = new System.Windows.Forms.Label();
             this.lblResult = new System.Windows.Forms.Label();
+            this.lblPlayer1Left = new System.Windows.Forms.Label();
+            this.lblPlayer1Right = new System.Windows.Forms.Label();
+            this.lblPcLeft = new System.Windows.Forms.Label();
+            this.lblPcRight = new System.Windows.Forms.Label();
+            this.lblWins = new System.Windows.Forms.Label();
+            this.lblLosses = new System.Windows.Forms.Label();
+            this.lblTies = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picRock)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPaper)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picScissors)).BeginInit();
@@ -80,6 +88,7 @@
             this.picPaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picPaper.TabIndex = 4;
             this.picPaper.TabStop = false;
+            this.picPaper.Click += new System.EventHandler(this.picPaper_Click);
             // 
             // picScissors
             // 
@@ -90,6 +99,7 @@
             this.picScissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picScissors.TabIndex = 5;
             this.picScissors.TabStop = false;
+            this.picScissors.Click += new System.EventHandler(this.picScissors_Click);
             // 
             // pictureBox4
             // 
@@ -121,29 +131,29 @@
             this.pictureBox6.TabIndex = 8;
             this.pictureBox6.TabStop = false;
             // 
-            // lblPlayer1
+            // lblPlayer1Middle
             // 
-            this.lblPlayer1.AutoSize = true;
-            this.lblPlayer1.BackColor = System.Drawing.Color.Transparent;
-            this.lblPlayer1.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlayer1.ForeColor = System.Drawing.Color.Lime;
-            this.lblPlayer1.Location = new System.Drawing.Point(357, 74);
-            this.lblPlayer1.Name = "lblPlayer1";
-            this.lblPlayer1.Size = new System.Drawing.Size(101, 31);
-            this.lblPlayer1.TabIndex = 9;
-            this.lblPlayer1.Text = "Player 1";
+            this.lblPlayer1Middle.AutoSize = true;
+            this.lblPlayer1Middle.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer1Middle.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1Middle.ForeColor = System.Drawing.Color.Lime;
+            this.lblPlayer1Middle.Location = new System.Drawing.Point(357, 74);
+            this.lblPlayer1Middle.Name = "lblPlayer1Middle";
+            this.lblPlayer1Middle.Size = new System.Drawing.Size(101, 31);
+            this.lblPlayer1Middle.TabIndex = 9;
+            this.lblPlayer1Middle.Text = "Player 1";
             // 
-            // lblPc
+            // lblPcMiddle
             // 
-            this.lblPc.AutoSize = true;
-            this.lblPc.BackColor = System.Drawing.Color.Transparent;
-            this.lblPc.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPc.ForeColor = System.Drawing.Color.Yellow;
-            this.lblPc.Location = new System.Drawing.Point(385, 291);
-            this.lblPc.Name = "lblPc";
-            this.lblPc.Size = new System.Drawing.Size(42, 31);
-            this.lblPc.TabIndex = 10;
-            this.lblPc.Text = "PC";
+            this.lblPcMiddle.AutoSize = true;
+            this.lblPcMiddle.BackColor = System.Drawing.Color.Transparent;
+            this.lblPcMiddle.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPcMiddle.ForeColor = System.Drawing.Color.Yellow;
+            this.lblPcMiddle.Location = new System.Drawing.Point(385, 291);
+            this.lblPcMiddle.Name = "lblPcMiddle";
+            this.lblPcMiddle.Size = new System.Drawing.Size(42, 31);
+            this.lblPcMiddle.TabIndex = 10;
+            this.lblPcMiddle.Text = "PC";
             // 
             // lblResult
             // 
@@ -151,11 +161,111 @@
             this.lblResult.BackColor = System.Drawing.Color.Transparent;
             this.lblResult.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblResult.ForeColor = System.Drawing.Color.White;
-            this.lblResult.Location = new System.Drawing.Point(12, 19);
+            this.lblResult.Location = new System.Drawing.Point(9, 16);
             this.lblResult.Name = "lblResult";
             this.lblResult.Size = new System.Drawing.Size(86, 31);
             this.lblResult.TabIndex = 11;
             this.lblResult.Text = "Result:";
+            // 
+            // lblPlayer1Left
+            // 
+            this.lblPlayer1Left.AutoSize = true;
+            this.lblPlayer1Left.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer1Left.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1Left.ForeColor = System.Drawing.Color.Lime;
+            this.lblPlayer1Left.Location = new System.Drawing.Point(144, 74);
+            this.lblPlayer1Left.Name = "lblPlayer1Left";
+            this.lblPlayer1Left.Size = new System.Drawing.Size(101, 31);
+            this.lblPlayer1Left.TabIndex = 12;
+            this.lblPlayer1Left.Text = "Player 1";
+            this.lblPlayer1Left.Visible = false;
+            // 
+            // lblPlayer1Right
+            // 
+            this.lblPlayer1Right.AutoSize = true;
+            this.lblPlayer1Right.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer1Right.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1Right.ForeColor = System.Drawing.Color.Lime;
+            this.lblPlayer1Right.Location = new System.Drawing.Point(579, 74);
+            this.lblPlayer1Right.Name = "lblPlayer1Right";
+            this.lblPlayer1Right.Size = new System.Drawing.Size(101, 31);
+            this.lblPlayer1Right.TabIndex = 13;
+            this.lblPlayer1Right.Text = "Player 1";
+            this.lblPlayer1Right.Visible = false;
+            // 
+            // lblPcLeft
+            // 
+            this.lblPcLeft.AutoSize = true;
+            this.lblPcLeft.BackColor = System.Drawing.Color.Transparent;
+            this.lblPcLeft.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPcLeft.ForeColor = System.Drawing.Color.Yellow;
+            this.lblPcLeft.Location = new System.Drawing.Point(170, 291);
+            this.lblPcLeft.Name = "lblPcLeft";
+            this.lblPcLeft.Size = new System.Drawing.Size(42, 31);
+            this.lblPcLeft.TabIndex = 14;
+            this.lblPcLeft.Text = "PC";
+            this.lblPcLeft.Visible = false;
+            // 
+            // lblPcRight
+            // 
+            this.lblPcRight.AutoSize = true;
+            this.lblPcRight.BackColor = System.Drawing.Color.Transparent;
+            this.lblPcRight.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPcRight.ForeColor = System.Drawing.Color.Yellow;
+            this.lblPcRight.Location = new System.Drawing.Point(606, 291);
+            this.lblPcRight.Name = "lblPcRight";
+            this.lblPcRight.Size = new System.Drawing.Size(42, 31);
+            this.lblPcRight.TabIndex = 15;
+            this.lblPcRight.Text = "PC";
+            this.lblPcRight.Visible = false;
+            // 
+            // lblWins
+            // 
+            this.lblWins.AutoSize = true;
+            this.lblWins.BackColor = System.Drawing.Color.Transparent;
+            this.lblWins.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWins.ForeColor = System.Drawing.Color.White;
+            this.lblWins.Location = new System.Drawing.Point(706, 447);
+            this.lblWins.Name = "lblWins";
+            this.lblWins.Size = new System.Drawing.Size(73, 31);
+            this.lblWins.TabIndex = 16;
+            this.lblWins.Text = "Wins:";
+            // 
+            // lblLosses
+            // 
+            this.lblLosses.AutoSize = true;
+            this.lblLosses.BackColor = System.Drawing.Color.Transparent;
+            this.lblLosses.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLosses.ForeColor = System.Drawing.Color.White;
+            this.lblLosses.Location = new System.Drawing.Point(705, 482);
+            this.lblLosses.Name = "lblLosses";
+            this.lblLosses.Size = new System.Drawing.Size(89, 31);
+            this.lblLosses.TabIndex = 17;
+            this.lblLosses.Text = "Losses:";
+            // 
+            // lblTies
+            // 
+            this.lblTies.AutoSize = true;
+            this.lblTies.BackColor = System.Drawing.Color.Transparent;
+            this.lblTies.Font = new System.Drawing.Font("Myriad Web Pro Condensed", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTies.ForeColor = System.Drawing.Color.White;
+            this.lblTies.Location = new System.Drawing.Point(706, 519);
+            this.lblTies.Name = "lblTies";
+            this.lblTies.Size = new System.Drawing.Size(62, 31);
+            this.lblTies.TabIndex = 18;
+            this.lblTies.Text = "Ties:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Black;
+            this.btnClose.ForeColor = System.Drawing.Color.Transparent;
+            this.btnClose.Location = new System.Drawing.Point(3, 528);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 31);
+            this.btnClose.TabIndex = 19;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // frmRockPaperScissors
             // 
@@ -164,9 +274,17 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(832, 561);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblTies);
+            this.Controls.Add(this.lblLosses);
+            this.Controls.Add(this.lblWins);
+            this.Controls.Add(this.lblPcRight);
+            this.Controls.Add(this.lblPcLeft);
+            this.Controls.Add(this.lblPlayer1Right);
+            this.Controls.Add(this.lblPlayer1Left);
             this.Controls.Add(this.lblResult);
-            this.Controls.Add(this.lblPc);
-            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.lblPcMiddle);
+            this.Controls.Add(this.lblPlayer1Middle);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
@@ -176,6 +294,7 @@
             this.Controls.Add(this.picRock);
             this.Name = "frmRockPaperScissors";
             this.Text = "Rock Paper Scissors";
+            this.Load += new System.EventHandler(this.frmRockPaperScissors_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picRock)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picPaper)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picScissors)).EndInit();
@@ -196,9 +315,17 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.Label lblPlayer1;
-        private System.Windows.Forms.Label lblPc;
+        private System.Windows.Forms.Label lblPlayer1Middle;
+        private System.Windows.Forms.Label lblPcMiddle;
         private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblPlayer1Left;
+        private System.Windows.Forms.Label lblPlayer1Right;
+        private System.Windows.Forms.Label lblPcLeft;
+        private System.Windows.Forms.Label lblPcRight;
+        private System.Windows.Forms.Label lblWins;
+        private System.Windows.Forms.Label lblLosses;
+        private System.Windows.Forms.Label lblTies;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- Part 6.5 - Rock Paper Scissors Project/Form1.cs
+++ Part 6.5 - Rock Paper Scissors Project/Form1.cs
@@ -11,7 +11,15 @@ using System.Windows.Forms;
 namespace Part_6._5___Rock_Paper_Scissors_Project
 {
     public partial class frmRockPaperScissors : Form
+
     {
+        Random generator = new Random();
+        int randomResult;
+        int wins = 0;
+        int losses = 0;
+        int ties = 0;
+
+
         public frmRockPaperScissors()
         {
             InitializeComponent();
@@ -19,10 +27,152 @@ namespace Part_6._5___Rock_Paper_Scissors_Project
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            if (picPaper.Click == true)
-            {
+            randomResult = generator.Next(1, 4);
 
+            lblPlayer1Left.Visible = true;
+            lblPlayer1Middle.Visible = false;
+            lblPlayer1Right.Visible = false;
+
+
+            if (randomResult == 1)
+            {
+                lblPcLeft.Visible = true;
+                lblPcMiddle.Visible = false;
+                lblPcRight.Visible = false;
+
+                lblResult.Text = ("Result: You Tied");
+                ties = ties + 1;
+                
             }
+
+            else if (randomResult == 2)
+            {
+                lblPcLeft.Visible = false;
+                lblPcMiddle.Visible = true;
+                lblPcRight.Visible = false;
+
+                lblResult.Text = ("Result: You Lost");
+                losses = losses + 1;
+            }
+
+            else if (randomResult == 3)
+            {
+                lblPcLeft.Visible = false;
+                lblPcMiddle.Visible = false;
+                lblPcRight.Visible = true;
+
+                lblResult.Text = ("Result: You Win");
+                wins = wins + 1;
+            }
+
+            lblWins.Text = ("Wins: " + wins);
+            lblLosses.Text = ("Losses: " + losses);
+            lblTies.Text = ("Ties: " + ties);
+
+
+
+
+        }
+
+        private void frmRockPaperScissors_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picPaper_Click(object sender, EventArgs e)
+        {
+            randomResult = generator.Next(1, 4);
+
+            lblPlayer1Left.Visible = false;
+            lblPlayer1Middle.Visible = true;
+            lblPlayer1Right.Visible = false;
+
+            if (randomResult == 1)
+            {
+                lblPcLeft.Visible = true;
+                lblPcMiddle.Visible = false;
+                lblPcRight.Visible = false;
+
+                lblResult.Text = ("Result: You Win");
+                wins = wins + 1;
+            }
+
+            else if (randomResult == 2)
+            {
+                lblPcLeft.Visible = false;
+                lblPcMiddle.Visible = true;
+                lblPcRight.Visible = false;
+
+                lblResult.Text = ("Result: You Tied");
+                ties = ties + 1;
+            }
+
+            else if (randomResult == 3)
+            {
+                lblPcLeft.Visible = false;
+                lblPcMiddle.Visible = false;
+                lblPcRight.Visible = true;
+
+                lblResult.Text = ("Result: You Lost");
+                losses = losses + 1;
+            }
+
+            lblWins.Text = ("Wins: " + wins);
+            lblLosses.Text = ("Losses: " + losses);
+            lblTies.Text = ("Ties: " + ties);
+
+        }
+
+        private void picScissors_Click(object sender, EventArgs e)
+        {
+            randomResult = generator.Next(1, 4);
+
+            lblPlayer1Left.Visible = false;
+            lblPlayer1Middle.Visible = false;
+            lblPlayer1Right.Visible = true;
+
+            if (randomResult == 1)
+            {
+                lblPcLeft.Visible = true;
+                lblPcMiddle.Visible = false;
+                lblPcRight.Visible = false;
+
+                lblResult.Text = ("Result: You Lost");
+                losses = losses + 1;
+            }
+
+            else if (randomResult == 2)
+            {
+                lblPcLeft.Visible = false;
+                lblPcMiddle.Visible = true;
+                lblPcRight.Visible = false;
+
+                lblResult.Text = ("Result: You Win");
+                wins = wins + 1;
+            }
+
+            else if (randomResult == 3)
+            {
+                lblPcLeft.Visible = false;
+                lblPcMiddle.Visible = false;
+                lblPcRight.Visible = true;
+
+                lblResult.Text = ("Result: You Tied");
+                ties = ties + 1;
+            }
+
+            lblWins.Text = ("Wins: " + wins);
+            lblLosses.Text = ("Losses: " + losses);
+            lblTies.Text = ("Ties: " + ties);
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
+
+
+
